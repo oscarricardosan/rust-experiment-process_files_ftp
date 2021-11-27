751434546d573f0b5286be963f813ef877350cfe
Oscar Sancheze <oscarricardosan@gmail.com>
2021-11-27 14:26:47 -0500

    COnfiguración de docker para desplegar ftp y pruebas de conección a FTP desde rust

--- app_rust/src/main.rs
+++ app_rust/src/main.rs
@@ -1,3 +1,40 @@
+extern crate ftp;
+
+use std::str;
+use std::io::Cursor;
+use ftp::FtpStream;
+
 fn main() {
-    println!("Hello, world!");
+    // Create a connection to an FTP server and authenticate to it.
+    let mut ftp_stream = FtpStream::connect("127.0.0.1:21").unwrap();
+    let _ = ftp_stream.login("savne", "savne").unwrap_or_else(|_e|{
+        panic!("Error de Usuario o contraseña");
+    });
+
+    let files= ftp_stream.nlst(Some("/"));
+    for file in files.as_ref().unwrap().iter(){
+        println!("{:?}", file);
+    }
+
+    println!("{:?}", files.unwrap());
+
+    // Get the current directory that the client will be reading from and writing to.
+    println!("Current directory: {}", ftp_stream.pwd().unwrap());
+
+    // Change into a new directory, relative to the one we are currently in.
+    let _ = ftp_stream.cwd("test_data").unwrap();
+
+    // Retrieve (GET) a file from the FTP server in the current working directory.
+    let remote_file = ftp_stream.simple_retr("ftpext-charter.txt").unwrap();
+    println!("Read file with contents\n{}\n", str::from_utf8(&remote_file.into_inner()).unwrap());
+
+    // Store (PUT) a file from the client to the current working directory of the server.
+    let mut reader = Cursor::new("Hello from the Rust \"ftp\" crate!".as_bytes());
+    let _ = ftp_stream.put("greeting.txt", &mut reader);
+    println!("Successfully wrote greeting.txt");
+
+    // Terminate the connection to the server.
+    let _ = ftp_stream.quit();
+
+
 }
